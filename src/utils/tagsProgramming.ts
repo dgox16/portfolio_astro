5f53e50b0ba1625faee4915d327371d252f5ff57
Diego Armando <dgox16@gmail.com>
2024-08-04 23:11:31 -0600

    Se termino de migrar la parte de proyectos

---
+++ src/utils/tagsProgramming.ts
@@ -0,0 +1,152 @@
+export const tags = {
+	react: {
+		name: "ReactJS",
+		icon: "react",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	svelte: {
+		name: "Svelte",
+		icon: "svelte",
+		color: "bg-latte-peach dark:bg-mocha-peach",
+	},
+	shadcnui: {
+		name: "Shadcn/ui",
+		icon: "shadcnui",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	tailwind: {
+		name: "TailwindCSS",
+		icon: "tailwindcss",
+		color: "bg-latte-sapphire dark:bg-mocha-sapphire",
+	},
+	astro: {
+		name: "Astro",
+		icon: "astro",
+		color: "bg-latte-red dark:bg-mocha-red",
+	},
+	bootstrap: {
+		name: "Bootstrap",
+		icon: "bootstrap",
+		color: "bg-mauve",
+	},
+	nextJs: {
+		name: "NextJS",
+		icon: "nextdotjs",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	django: {
+		name: "Django",
+		icon: "django",
+		color: "bg-latte-green dark:bg-mocha-green",
+	},
+	jinja: {
+		name: "Jinja",
+		icon: "jinja",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	mySql: {
+		name: "MySQL",
+		icon: "mysql",
+		color: "bg-latte-peach dark:bg-mocha-peach",
+	},
+	sqlite: {
+		name: "SQLite",
+		icon: "sqlite",
+		color: "bg-lavender",
+	},
+	mongo: {
+		name: "MongoDB",
+		icon: "mongodb",
+		color: "bg-latte-green dark:bg-mocha-green",
+	},
+	postgresql: {
+		name: "PostgreSQL",
+		icon: "postgresql",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	express: {
+		name: "ExpressJS",
+		icon: "express",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	supabase: {
+		name: "Supabase",
+		icon: "supabase",
+		color: "bg-latte-green dark:bg-mocha-green",
+	},
+	javascript: {
+		name: "JavaScript",
+		icon: "javascript",
+		color: "bg-yellow",
+	},
+	typescript: {
+		name: "TypeScript",
+		icon: "typescript",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	python: {
+		name: "Python",
+		icon: "python",
+		color: "bg-yellow",
+	},
+	html: {
+		name: "HTML",
+		icon: "html5",
+		color: "bg-latte-peach dark:bg-mocha-peach",
+	},
+	rust: {
+		name: "Rust",
+		icon: "rust",
+		color: "bg-latte-peach dark:bg-mocha-peach",
+	},
+	css: {
+		name: "CSS",
+		icon: "css3",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	bash: {
+		name: "Bash",
+		icon: "gnubash",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	lua: {
+		name: "Lua",
+		icon: "lua",
+		color: "bg-latte-sapphire dark:bg-mocha-sapphire",
+	},
+	git: {
+		name: "Git",
+		icon: "git",
+		color: "bg-latte-peach dark:bg-mocha-peach",
+	},
+	linux: {
+		name: "Linux",
+		icon: "linux",
+		color: "bg-latte-text dark:bg-mocha-text",
+	},
+	nodeJs: {
+		name: "NodeJS",
+		icon: "nodedotjs",
+		color: "bg-latte-green dark:bg-mocha-green",
+	},
+	vscode: {
+		name: "VSCode",
+		icon: "vscode",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	neovim: {
+		name: "Neovim",
+		icon: "neovim",
+		color: "bg-latte-green dark:bg-mocha-green",
+	},
+	docker: {
+		name: "Docker",
+		icon: "docker",
+		color: "bg-latte-blue dark:bg-mocha-blue",
+	},
+	npm: {
+		name: "NPM",
+		icon: "npm",
+		color: "bg-latte-red dark:bg-mocha-red",
+	},
+};
